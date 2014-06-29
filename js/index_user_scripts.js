25e8f70e4995c637f703ca9c85c760a8be40f762
bltsandwich1 <itcaughtonj@gmail.com>
2014-06-28 19:57:17 -0500

    0.0.1
    More structure.

--- js/index_user_scripts.js
+++ js/index_user_scripts.js
@@ -12,6 +12,35 @@
         {
          activate_subpage("#AboutUs"); 
         });
+        $(document).on("click", ".uib_w_31", function(evt)
+        {
+         activate_subpage("#BasicMath"); 
+        });
+        $(document).on("click", ".uib_w_37", function(evt)
+        {
+         activate_subpage("#ForLoops"); 
+        });
+        $(document).on("click", ".uib_w_38", function(evt)
+        {
+         activate_subpage("#WhileLoops"); 
+        });
+        $(document).on("click", ".uib_w_39", function(evt)
+        {
+         activate_subpage("#IfElseStatements"); 
+        });
+        $(document).on("click", ".uib_w_32", function(evt)
+        {
+         activate_subpage("#LogicalOperations"); 
+        });
+        $(document).on("click", ".uib_w_33", function(evt)
+        {
+         activate_subpage("#MatrixOperations"); 
+        });
+        $(document).on("click", ".uib_w_35", function(evt)
+        {
+         activate_subpage("#CommonFunctions"); 
+        });
+        
 }
  $(document).ready(register_event_handlers);
 })();
