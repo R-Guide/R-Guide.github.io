b654c10715b73809f64ac40392b873b03ed303ee
bltsandwich1 <itcaughtonj@gmail.com>
2014-06-30 15:55:38 -0500

    0.1.0

--- js/index_user_scripts.js
+++ js/index_user_scripts.js
@@ -41,6 +41,63 @@
          activate_subpage("#CommonFunctions"); 
         });
         
+        $(document).on("click", ".uib_w_41", function(evt)
+        {
+         activate_subpage("#FunctionSyntax"); 
+        });
+        $(document).on("click", ".uib_w_42", function(evt)
+        {
+         activate_subpage("#CallingFunctions"); 
+        });
+        $(document).on("click", ".uib_w_43", function(evt)
+        {
+         activate_subpage("#GoodPractice"); 
+        });
+        
+        $(document).on("click", "#FunctionBack", function(evt)
+        {
+         activate_subpage("#item5_pagesub"); 
+        });
+        $(document).on("click", "#CallingBack", function(evt)
+        {
+         activate_subpage("#item5_pagesub"); 
+        });
+        $(document).on("click", "#PracticeBack", function(evt)
+        {
+         activate_subpage("#item5_pagesub"); 
+        });
+        $(document).on("click", "#IfElseBack", function(evt)
+        {
+         activate_subpage("#item4_pagesub"); 
+        });
+        $(document).on("click", "#Back42", function(evt)
+        {
+         activate_subpage("#item4_pagesub"); 
+        });
+        $(document).on("click", "#BackFor", function(evt)
+        {
+         activate_subpage("#item4_pagesub"); 
+        });
+        $(document).on("click", "#CommonBack", function(evt)
+        {
+         activate_subpage("#item3_pagesub"); 
+        });
+        $(document).on("click", ".uib_w_64", function(evt)
+        {
+         activate_subpage("#item3_pagesub"); 
+        });
+        $(document).on("click", ".uib_w_61", function(evt)
+        {
+         activate_subpage("#item3_pagesub"); 
+        });
+        $(document).on("click", "#BasicMathBack", function(evt)
+        {
+         activate_subpage("#item3_pagesub"); 
+        });
+        $(document).on("click", ".uib_w_111", function(evt)
+        {
+        /* your code goes here */ 
+        });
 }
  $(document).ready(register_event_handlers);
 })();
